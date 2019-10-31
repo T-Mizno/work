727db007129abcb37b9ab6a608d82309b3482f32
T.Mizno <of.mizno@gmail.com>
2019-11-01 02:59:11 +0900

    garden sample.

---
+++ 20191031-img2array.py
@@ -0,0 +1,47 @@
+from PIL import Image
+import numpy as np
+
+
+M = 80
+N = M
+
+# 元となる画像の読み込み
+imgOrigin = Image.open('./gardenPlan.jpg')
+
+img = imgOrigin.resize((M, N), Image.LANCZOS)
+
+
+img.save("out.jpg")
+
+arr = np.array(img)
+
+w, h, c = arr.shape
+
+strs = []
+for j in range(w):
+    for i in range(h):
+        chroma = '0x%02X%02X%02X' % (arr[i][j][0], arr[i][j][1], arr[i][j][2])
+        chromaStr = ""+chroma
+        # strs.append(chromaStr.lower())
+        strs.append(chromaStr)
+
+
+print('let imgDataStr="{', end="")
+print('\\"m\\":', M, ',', end="")
+print('\\"n\\":', N, ',', end="")
+print('\\"data\\":[', '\\"'+strs[0]+'\\"', end="")
+for i in range(1, len(strs)):
+    print(',', '\\"'+strs[i]+'\\"', end="")
+print(']}";')
+
+print('''
+let imgData = JSON.parse(imgDataStr);
+let img = [];
+for (let i = 0; i < imgData["m"]; i++) {
+    let row = [];
+    for (let j = 0; j < imgData["n"]; j++) {
+        row.push(imgData["data"][i * imgData["m"] + j]);
+    }
+    img.push(row);
+}
+''')
